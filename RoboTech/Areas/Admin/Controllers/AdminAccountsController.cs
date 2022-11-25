64ee73f84d3d19f4bf27b26be373fd97c9275bc2
Nguyen Quoc Khanh <20521452@gm.uit.edu.vn>
2022-11-25 15:23:32 +0700

    fix account admin models

--- RoboTech/Areas/Admin/Controllers/AdminAccountsController.cs
+++ RoboTech/Areas/Admin/Controllers/AdminAccountsController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RoboTech.Models;
+using RoboTech.ModelViews;
 
 namespace RoboTech.Areas.Admin.Controllers
 {
@@ -20,13 +21,14 @@ namespace RoboTech.Areas.Admin.Controllers
     {
         private readonly shoplaptopContext _context;
         public INotyfService _notyfService { get; }
-        public AdminAccountsController(shoplaptopContext context, , INotyfService notyfService)
+        public AdminAccountsController(shoplaptopContext context, INotyfService notyfService)
         {
             _context = context;
             _notyfService = notyfService;
         }
 
         // GET: Admin/AdminAccounts
+
         public async Task<IActionResult> Index()
         {
             ViewData["QuyenTruyCap"] = new SelectList(_context.TbRoles, "RoleId", "Description");
@@ -34,34 +36,36 @@ namespace RoboTech.Areas.Admin.Controllers
             lsTrangThai.Add(new SelectListItem() { Text = "Hoạt động", Value = "1" });
             lsTrangThai.Add(new SelectListItem() { Text = "Khóa", Value = "0" });
             ViewData["lsTrangThai"] = lsTrangThai;
-            
-            var shoplaptopContext = _context.TbUsers.Include(t => t.Role);
-            return View(await shoplaptopContext.ToListAsync());
+
+
+
+            var dbMarketsContext = _context.TbUsers.Include(a => a.Role);
+            return View(await dbMarketsContext.ToListAsync());
         }
 
         // GET: Admin/AdminAccounts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.TbUsers == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var tbUser = await _context.TbUsers
-                .Include(t => t.Role)
+            var account = await _context.TbUsers
+                .Include(a => a.Role)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (tbUser == null)
+            if (account == null)
             {
                 return NotFound();
             }
 
-            return View(tbUser);
+            return View(account);
         }
 
         // GET: Admin/AdminAccounts/Create
         public IActionResult Create()
         {
-            ViewData["RoleId"] = new SelectList(_context.TbRoles, "RoleId", "RoleId");
+            ViewData["QuyenTruyCap"] = new SelectList(_context.TbRoles, "RoleId", "RoleName");
             return View();
         }
 
@@ -70,34 +74,64 @@ namespace RoboTech.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Username,Password,Address,RoleId,LastLogin,CreateDate,Active,Phone,Email,Salt")] Account account)
+        public async Task<IActionResult> Create([Bind("AccountId,Phone,Email,Password,Salt,Active,FullName,RoleId,LastLogin,CreateDate")] TbUser account)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(tbUser);
+
+                _context.Add(account);
                 await _context.SaveChangesAsync();
                 _notyfService.Success("Tạo mới tài khoản thành công");
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["QuyenTruyCap"] = new SelectList(_context.Roles, "RoleId", "RoleName", account.RoleId);
+            ViewData["QuyenTruyCap"] = new SelectList(_context.TbRoles, "RoleId", "RoleName", account.RoleId);
             return View(account);
         }
+        /*//ChangePassword
+        public IActionResult ChangePassword()
+        {
+            ViewData["QuyenTruyCap"] = new SelectList(_context.TbRoles, "RoleId", "RoleName");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var taikhoan = _context.TbUsers.AsNoTracking().SingleOrDefault(x => x.Email == model.Email);
+                if (taikhoan == null) return RedirectToAction("Login", "Accounts");
+                var pass = (model.PasswordNow.Trim() + taikhoan.Salt.Trim()).ToMD5();
+                {
+                    string passnew = (model.Password.Trim() + taikhoan.Salt.Trim()).ToMD5();
+                    taikhoan.Password = passnew;
+                    taikhoan.LastLogin = DateTime.Now;
+                    _context.Update(taikhoan);
+                    _context.SaveChanges();
+                    _notyfService.Success("Đổi mật khẩu thành công");
+                    return RedirectToAction("Login", "Accounts", new { Area = "Admin" });
+                }
+            }
+
+
+            return View();
+        }*/
 
         // GET: Admin/AdminAccounts/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.TbUsers == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var tbUser = await _context.TbUsers.FindAsync(id);
-            if (tbUser == null)
+            var account = await _context.TbUsers.FindAsync(id);
+            if (account == null)
             {
                 return NotFound();
             }
-            ViewData["RoleId"] = new SelectList(_context.TbRoles, "RoleId", "RoleId", tbUser.RoleId);
-            return View(tbUser);
+            ViewData["QuyenTruyCap"] = new SelectList(_context.TbRoles, "RoleId", "RoleName", account.RoleId);
+            return View(account);
         }
 
         // POST: Admin/AdminAccounts/Edit/5
@@ -105,9 +139,9 @@ namespace RoboTech.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Username,Password,Address,RoleId,LastLogin,CreateDate,Active,Phone,Email,Salt")] TbUser tbUser)
+        public async Task<IActionResult> Edit(int id, [Bind("AccountId,Phone,Email,Password,Salt,Active,FullName,RoleId,LastLogin,CreateDate")] TbUser account)
         {
-            if (id != tbUser.Id)
+            if (id != account.Id)
             {
                 return NotFound();
             }
@@ -116,12 +150,12 @@ namespace RoboTech.Areas.Admin.Controllers
             {
                 try
                 {
-                    _context.Update(tbUser);
+                    _context.Update(account);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!TbUserExists(tbUser.Id))
+                    if (!AccountExists(account.Id))
                     {
                         return NotFound();
                     }
@@ -132,27 +166,27 @@ namespace RoboTech.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["RoleId"] = new SelectList(_context.TbRoles, "RoleId", "RoleId", tbUser.RoleId);
-            return View(tbUser);
+            ViewData["QuyenTruyCap"] = new SelectList(_context.TbRoles, "RoleId", "RoleName", account.RoleId);
+            return View(account);
         }
 
         // GET: Admin/AdminAccounts/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.TbUsers == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var tbUser = await _context.TbUsers
-                .Include(t => t.Role)
+            var account = await _context.TbUsers
+                .Include(a => a.Role)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (tbUser == null)
+            if (account == null)
             {
                 return NotFound();
             }
 
-            return View(tbUser);
+            return View(account);
         }
 
         // POST: Admin/AdminAccounts/Delete/5
@@ -160,23 +194,17 @@ namespace RoboTech.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.TbUsers == null)
-            {
-                return Problem("Entity set 'shoplaptopContext.TbUsers'  is null.");
-            }
-            var tbUser = await _context.TbUsers.FindAsync(id);
-            if (tbUser != null)
-            {
-                _context.TbUsers.Remove(tbUser);
-            }
-            
+            var account = await _context.TbUsers.FindAsync(id);
+            _context.TbUsers.Remove(account);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool TbUserExists(int id)
+        private bool AccountExists(int id)
         {
-          return _context.TbUsers.Any(e => e.Id == id);
+            return _context.TbUsers.Any(e => e.Id == id);
         }
+
     }
+
 }
