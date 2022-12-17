e31866d3d106d772081de92ed92e5a042622feb8
Nhanna <19521940@gm.uit.edu.vn>
2022-12-17 12:26:17 +0700

    restore banner and slider on home page

--- RoboTech/Models/shoplaptopContext.cs
+++ RoboTech/Models/shoplaptopContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace RoboTech.Models
 {
@@ -32,7 +29,6 @@ namespace RoboTech.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=ELONMUSK;Database=shoplaptop;Trusted_Connection=True;");
             }
         }
