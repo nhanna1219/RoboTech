e15fc4609685e170cca3062349a80f5e944b79de
Nguyen Quoc Khanh <20521452@gm.uit.edu.vn>
2022-12-29 06:13:44 +0700

    update some view more beautifull

---
+++ RoboTech/Areas/Admin/Controllers/AdminOrdersController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PagedList.Core;
+using RoboTech.Models;
+
+namespace RoboTech.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly shoplaptopContext _context;
+        public INotyfService _notyfService { get; }
+        public AdminOrdersController(shoplaptopContext context, INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+
+        // GET: Admin/AdminOrders
+
+        public IActionResult Index(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 20;
+            var Orders = _context.TbOrders.Include(o => o.Customer)
+                /*.Include(o => o.TransactStatus)*/
+                .AsNoTracking()
+                .OrderBy(x => x.OrderDate);
+            PagedList<TbOrder> models = new PagedList<TbOrder>(Orders, pageNumber, pageSize);
+
+            ViewBag.CurrentPage = pageNumber;
+
+
+
+            return View(models);
+        }
+
+        // GET: Admin/AdminOrders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.TbOrders
+                .Include(o => o.Customer)
+               /* .Include(o => o.TransactStatus)*/
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var Chitietdonhang = _context.TbOrderDetails
+                .Include(x => x.Product)
+                .AsNoTracking()
+                .Where(x => x.OrderId == order.Id)
+                .OrderBy(x => x.OrderDetailId)
+                .ToList();
+            ViewBag.ChiTiet = Chitietdonhang;
+            return View(order);
+        }
+
+
+        /*public async Task<IActionResult> ChangeStatus(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.TbOrders
+                .AsNoTracking()
+                .Include(x => x.Customer)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["Trangthai"] = new SelectList(_context.TransactStatuses, "TransactStatusId", "Status", order.TransactStatusId);
+            return PartialView("ChangeStatus", order);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangeStatus(int id, [Bind("OrderId,CustomerId,OrderDate,ShipDate,TransactStatusId,Deleted,Paid,PaymentDate,TotalMoney,PaymentId,Note,Address,LocationId,District,Ward")] Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var donhang = await _context.Orders.AsNoTracking().Include(x => x.Customer).FirstOrDefaultAsync(x => x.OrderId == id);
+                    if (donhang != null)
+                    {
+                        donhang.Paid = order.Paid;
+                        donhang.Deleted = order.Deleted;
+                        donhang.TransactStatusId = order.TransactStatusId;
+                        if (donhang.Paid == true)
+                        {
+                            donhang.PaymentDate = DateTime.Now;
+                        }
+                        if (donhang.TransactStatusId == 5) donhang.Deleted = true;
+                        if (donhang.TransactStatusId == 3) donhang.ShipDate = DateTime.Now;
+                    }
+                    _context.Update(donhang);
+                    await _context.SaveChangesAsync();
+                    _notyfService.Success("Cập nhật trạng thái đơn hàng thành công");
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Trangthai"] = new SelectList(_context.TransactStatuses, "TransactStatusId", "Status", order.TransactStatusId);
+            return PartialView("ChangeStatus", order);
+        }
+*/
+        // GET: Admin/AdminOrders/Create
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var order = await _context.TbOrders
+                .Include(o => o.Customer)
+               /* .Include(o => o.TransactStatus)*/
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var Chitietdonhang = _context.TbOrderDetails
+                .Include(x => x.Product)
+                .AsNoTracking()
+                .Where(x => x.OrderDetailId == order.Id)
+                .OrderBy(x => x.OrderDetailId)
+                .ToList();
+            ViewBag.ChiTiet = Chitietdonhang;
+
+            return View(order);
+        }
+
+        // POST: Admin/AdminOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.TbOrders.FindAsync(id);
+            order.Deleted = true;
+            _context.Update(order);
+            await _context.SaveChangesAsync();
+            _notyfService.Success("Xóa đơn hàng thành công");
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.TbOrders.Any(e => e.Id == id);
+        }
+
+    }
+}
